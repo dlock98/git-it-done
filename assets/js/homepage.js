f43ef1095349b72178470fed5560bc4daf4ba054
Danielle <danielle@Danielles-MacBook-Air.local>
2022-07-10 17:36:39 -0400

    editing

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -19,16 +19,18 @@ if (username) {
   response.json().then(function(data) {
     displayRepos(data, user);
   });
-  fetch(apiUrl)
-  .then(function(response) {
+  fetch(apiUrl).then(function(response) {
+    // request was successful
     if (response.ok) {
       response.json().then(function(data) {
-        displayRepos(data, user);
+        // pass response data to dom function
+        displayIssues(data);
       });
-    } else {
-      alert("Error: GitHub User Not Found");
     }
-    })
+    else {
+      alert("There was a problem with your request!");
+    }
+  })
     .catch(function(error) {
     // Notice this `.catch()` getting chained onto the end of the `.then()` method
     alert("Unable to connect to GitHub");
@@ -48,15 +50,17 @@ var displayRepos = function(repos, searchTerm) {
     repoSearchTerm.textContent = searchTerm;
 
     // loop over repos
+// loop over repos
 for (var i = 0; i < repos.length; i++) {
     // format repo name
     var repoName = repos[i].owner.login + "/" + repos[i].name;
   
-    // create a container for each repo
-    var repoEl = document.createElement("div");
-    repoEl.classList = "list-item flex-row justify-space-between align-center";
-  
+    // create a link for each repo
+var repoEl = document.createElement("a");
+repoEl.classList = "list-item flex-row justify-space-between align-center";
+repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
     // create a span element to hold repository name
+
     var titleEl = document.createElement("span");
     titleEl.textContent = repoName;
   
